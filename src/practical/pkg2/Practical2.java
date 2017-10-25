ec9f863cc0dcde85902b2e24ea9596394aa2a4a6
B1 <B1@B152>
2017-10-25 13:50:36 +0800

    commit

---
+++ src/practical/pkg2/Practical2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical.pkg2;
+
+/**
+ *
+ * @author B1
+ */
+public class Practical2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
