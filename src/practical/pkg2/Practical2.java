22576a9e8e66591f3d71c30f48712afaf4295328
B1 <B1@B152>
2017-10-25 14:20:31 +0800

    commit

--- src/practical/pkg2/Practical2.java
+++ src/practical/pkg2/Practical2.java
@@ -15,7 +15,7 @@ public class Practical2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Hello");
     }
     
 }
